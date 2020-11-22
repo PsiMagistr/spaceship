dff3b7ba183cec134a2d64acda36a53f4e243847
Pavel <chago81@inbox.ru>
2020-11-22 09:47:03 +0200

    Последний

--- js/shooter.js
+++ js/shooter.js
@@ -29,7 +29,7 @@ window.addEventListener("load", function(){
         Shoot:function(){//Метод стрельбы.
             if(this.IsShooting == false){
                this.IsShooting = true;
-               this.bullets.push(new Bullet());
+               this.bullets.push(new Bullet(9, 545), new Bullet(33, 545));
                this.sound.play();  
             }
         },
@@ -72,6 +72,7 @@ window.addEventListener("load", function(){
         this.X = Rnd(0, canvas.width / this.Size) * this.Size; // Координаты по Х
         this.Y = -this.Size; //Начальные координаты по У
         this.Speed = Rnd(1,5);//Скорость движения астероида от 1 до 5
+        this.isShoot = false;
         this.Del = false;//Флаг удаления.                
         this.Move = function(){//Движение астероида, метод
             this.Y += this.Speed;
@@ -81,10 +82,10 @@ window.addEventListener("load", function(){
         }
     }
     
-    function Bullet(){ // Конструктор объекта пули.
-        this.Size = 10;
-        this.X = gun.X + gun.width / 2 - this.Size / 2;
-        this.Y = 560;
+    function Bullet(dx, y){ // Конструктор объекта пули.
+        this.Size = 8;
+        this.X = gun.X + dx; //9;//dx; /*gun.width / 2 - this.Size / 2*/;
+        this.Y = y;//545;
         this.Speed = 3;
         this.Del = false;
         this.Move = function(){
@@ -92,28 +93,47 @@ window.addEventListener("load", function(){
             if(this.Y < 0){
                this.Del = true; 
             }
+            else{
+                for(var i = 0; i < asteroids.length; i++){
+                   if((this.X >= asteroids[i].X) &&
+                   (this.X <= asteroids[i].X + asteroids[i].Size - this.Size) &&
+                   (this.Y >=  asteroids[i].Y ) &&
+                   (this.Y <= asteroids[i].Y + asteroids[i].Size - this.Size)){
+                      asteroids[i].isShoot = true;  
+                      asteroids[i].Del = true;
+                      this.Del = true;                      
+                      bum.play();                                          
+                      break;
+                   } 
+                }               
+            }            
         }
     }
     
-    
-
-    function update(){ //Обновление мира.
-        gun.BlasterDelay(); //Задержка выстрела бластерной пушки.
+    function generatorAsteroids(){
         speedaster--;
         if(speedaster == 0){ //Генерация астероидов.
             speedaster = 100;
             asteroids.push(new Asteroid());
         }
+    }
 
+    function update(){ //Обновление мира.
+        gun.BlasterDelay(); //Задержка выстрела бластерной пушки.
+        generatorAsteroids();
         for(var i in asteroids){ // Двигаем астероиды.
-            asteroids[i].Move();
-        }
-        
+            asteroids[i].Move();            
+        }     
+       
         for(var i in gun.bullets){ //Двигаем все пульки
             gun.bullets[i].Move();
         }
-
-        for(var i in gun.bullets){ // Проверка коллизий и установка коллизионных флагов.
+        for(var i in asteroids){ 
+            if(asteroids[i].isShoot){                             
+               gun.Count++;                       
+            }   
+        } 
+       /* for(var i in gun.bullets){ // Проверка коллизий и установка коллизионных флагов.
             for(var j in asteroids){
                 if((gun.bullets[i].X >= asteroids[j].X) &&
                 (gun.bullets[i].X <= asteroids[j].X + asteroids[j].Size - gun.bullets[i].Size) &&
@@ -125,16 +145,18 @@ window.addEventListener("load", function(){
                     bum.play();
                 } 
             }            
-        }    
+        }*/    
 
         asteroids = clearAll(asteroids);        
-        gun.bullets = clearAll(gun.bullets);
+        gun.bullets = clearAll(gun.bullets);       
+        
+        
     }
 
     function render(){ // функция отрисовки
        scena.drawImage(fon, 0, 0, canvas.width, canvas.height);
        scena.drawImage(gun.sprite, gun.X, gun.Y, gun.width, gun.width);
-       scena.fillStyle = "red";
+       scena.fillStyle = "#00FA9A";
        for(var i in gun.bullets){
           scena.fillRect(gun.bullets[i].X, gun.bullets[i].Y, gun.bullets[i].Size, gun.bullets[i].Size);
        }
